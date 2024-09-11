81061c401fc492a68b352d4e9f6c8789ad864c72
Brad Hesson <brad.hesson@outlook.com>
2024-09-11 15:49:55 -0600

    added flake to navigator

---
+++ ABDNavigator/flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = flakes: flakes.flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import flakes.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      settingsFile = pkgs.writeText "settings.json" (builtins.toJSON {
+        "java.jdt.ls.java.home" = "${pkgs.jdk}";
+      });
+    in
+    {
+      devShell = with pkgs; mkShell {
+        packages = [
+          jdk8
+        ];
+        buildInputs = [
+        ];
+        shellHook = ''
+          mkdir .vscode
+          cp -f ${settingsFile} .vscode/settings.json
+        '';
+      };
+    }
+  );
+}
+
